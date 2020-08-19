12a834d1d9eb63de659c03c58f00c522ed29944f
kapillava <69887504+kapillava@users.noreply.github.com>
2020-08-19 11:06:41 +0530

    Update BaseEntity.java

--- src/main/java/com/org/kapil/modal/BaseEntity.java
+++ src/main/java/com/org/kapil/modal/BaseEntity.java
@@ -10,9 +10,7 @@ import lombok.Getter;
 
 @Entity
 @Getter
-// Choose your inheritance strategy:
-//@Inheritance(strategy=InheritanceType.JOINED)
-//@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+
 @Inheritance(strategy=InheritanceType.TABLE_PER_CLASS)
 public abstract class BaseEntity {
     @Id
